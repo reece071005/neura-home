b80e152b0bb9be59848f85ef110603da55668b74
Seyed Erfan Alavian <erfanalavian2005@gmail.com>
2026-01-15 13:29:30 +0400

    testing

--- app/(drawer)/adminPage.tsx
+++ app/(drawer)/adminPage.tsx
@@ -1,12 +1,14 @@
-import { View, ScrollView } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const AdminPage =()  => {
     return (
         <SafeAreaView className="flex-1 bg-white">
             <ScrollView className="flex-1 px-4">
-                <View className="py-4">
-                    {/*content goes here*/}
+                <View className="flex-row items-center px-6 py-4">
+                    <Text className="text-2xl font-bold text-black-900">
+                        Admin Page
+                    </Text>
                 </View>
             </ScrollView>
         </SafeAreaView>
