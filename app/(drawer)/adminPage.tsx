f2ae5004abc318e933080ff3d91b9c07c37ce540
Seyed Erfan Alavian <erfanalavian2005@gmail.com>
2026-01-24 12:21:15 +0400

    12pm

--- app/(drawer)/adminPage.tsx
+++ app/(drawer)/adminPage.tsx
@@ -5,11 +5,19 @@ const AdminPage =()  => {
     return (
         <SafeAreaView className="flex-1 bg-white">
             <ScrollView className="flex-1 px-4">
-                <View className="flex-row items-center px-6 py-4">
+                <View className="flex-row justify-center items-center">
                     <Text className="text-2xl font-bold text-black-900">
                         Admin Page
                     </Text>
                 </View>
+
+                <View className="w-full items-start mt-4">
+                    <Text className="text-lg font-bold text-black-800">
+                        Accounts Connected
+                    </Text>
+                </View>
+
+                <View className="w-full border-b bg-black-300 mt-2 mb-4" />
             </ScrollView>
         </SafeAreaView>
     );
