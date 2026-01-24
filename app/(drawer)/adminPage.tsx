bbef525974dedd5436e38c3785d431b7f00185b3
Seyed Erfan Alavian <erfanalavian2005@gmail.com>
2026-01-24 13:06:18 +0400

    Added accounts connected section

--- app/(drawer)/adminPage.tsx
+++ app/(drawer)/adminPage.tsx
@@ -17,7 +17,20 @@ const AdminPage =()  => {
                     </Text>
                 </View>
 
+                {/*Line Divider*/}
                 <View className="w-full border-b bg-black-300 mt-2 mb-4" />
+
+                    {/*User 4*/}
+                    <View className="flex-row items-center p-4 border-b border-gray-100 border-[0.5px]">
+                        <View className="w-10 h-10 bg-gray-100 rounded-full" />
+                        <View className="flex-1 ml-4">
+                            <Text className="text-base font-bold text-black-900">User 4</Text>
+                            <Text className="text-xs text-gray-500">Connected 2 Weeks Ago - 10/11/2025</Text>
+                        </View>
+                        <View className="w-8 h-8" />
+                    </View>
+
+
             </ScrollView>
         </SafeAreaView>
     );
